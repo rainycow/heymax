a24c4fcbae8d1da420657c45b572c623d52c78ef
tyyy <tlu002@e.ntu.edu.sg>
2025-06-20 01:05:11 +0800

    update dim_user model

--- dbt/models/silver/dim_users_silver.sql
+++ dbt/models/silver/dim_users_silver.sql
@@ -1,16 +1,5 @@
-SELECT
+SELECT DISTINCT
     *, 
     '{{ run_started_at.strftime("%Y-%m-%d") }}' as loaded_at
 FROM
     {{ ref('dim_users_bronze') }} bronze
-
-{% if is_incremental() %}
-WHERE
-    bronze.loaded_at > (
-        SELECT
-            max(loaded_at)
-        FROM
-            {{this}}
-    )
-
-{% endif %}
--- dbt/models/silver/fact_events_silver.sql
+++ dbt/models/silver/fact_events_silver.sql
@@ -3,14 +3,3 @@ SELECT
     '{{ run_started_at.strftime("%Y-%m-%d") }}' as loaded_at
 FROM
     {{ ref('fact_events_bronze') }} bronze
-
-{% if is_incremental() %}
-WHERE
-    bronze.loaded_at > (
-        SELECT
-            max(loaded_at)
-        FROM
-            {{this}}
-    )
-
-{% endif %}
